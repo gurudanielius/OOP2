54fa6a15f01d7e5cf5a2e177f3b9bc8b7d06afa5
Daniel Gurvič <gurvic.danielius@gmail.com>
2024-09-15 23:08:49 +0300

    Pridėtos bibliotekos į header.h
    Pridėta random, vector,random, iomanip ir algorithm bibliotekos, skirtos atitinkamai naudoti vektorius, funkcijas: setprecision, sort ir rand

--- header.h
+++ header.h
@@ -1,13 +1,18 @@
 //
-// Created by danie on 15/09/2024.
+// Created by danie on 13/09/2024.
 //
-#include <iostream>
+#include <random>
+#include <vector>
 using std::endl;
 using std::cout;
 using std::cin;
 using std::string;
-
+using std::vector;
 #ifndef HEADER_H
 #define HEADER_H
-
+#include <iomanip>
+using std::setw;
+using std::fixed;
+using std::setprecision;
+#include <algorithm>
 #endif //HEADER_H
