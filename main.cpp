ca374c7746f85ad74ba5aa05c7e3bb4c20ab3c57
Daniel Gurvič <gurvic.danielius@gmail.com>
2024-09-16 01:00:39 +0300

    Medianos/Vidurkio pasirinkimo pridėjimas

--- main.cpp
+++ main.cpp
@@ -1,7 +1,43 @@
-#include <iostream>
+#include "header.h"
+#include "Struct.h"'
+string choice;
+int main() {
+    vector<Stud> v1;
+    Stud Temp;
+    int n;
+    cout<<"Iveskite studentu skaiciu"<<endl;
+    cin>>n;
+    cout<<"Pasirinkite ka skaiciuoti: V - vidurki, M - mediana"<<endl;
+    cin>>choice;
+    for (int i=0;i<n;i++) {
+        cout<<"Iveskite duomenis"<<endl;
+        ived(Temp);
+        v1.push_back(Temp);
+        val(Temp);
+    }
+    if (choice=="V") {
+        cout << std::setw(10) << "Vardas" << "    "
+             << std::setw(10) << "Pavarde" << "    "
+             << std::setw(10) << "Galutinis (Vid.)" << endl;
+    }
+    else {
+        cout << std::setw(10) << "Vardas" << "    "
+             << std::setw(10) << "Pavarde" << "    "
+             << std::setw(10) << "Galutinis (Med.)" << endl;
+    }
+    cout << "-----------------------------------------------" << endl;
 
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
+
+
+    for (int i=0;i<n;i++) {
+        output(v1.at(i));
+    }
+
+
+
+    system("pause");
+
     return 0;
 }
+
+
