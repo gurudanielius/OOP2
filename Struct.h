51feb75e0ac5b2e5b2a119b7ff088c94773a91f2
Daniel Gurvič <gurvic.danielius@gmail.com>
2024-09-19 01:08:30 +0300

    Update Struct.h

--- Struct.h
+++ Struct.h
@@ -2,12 +2,15 @@
     struct Stud {
         string vardas;
         string pavarde;
-        vector<double> ND;       
+        vector<double> ND;
         double vid;              
         double med;              
         double egz;              
         double rez;              
     };
     void ived(Stud & Lok);
+    void vidurkis(Stud & Lok);
     void output(Stud Lok);
     void val(Stud & Lok);
+    void mediana(Stud & Lok);
+    void output2(vector<Stud> Lok);
