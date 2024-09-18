52b0722ea6ce21cff6d0f3662625678b274dd036
Daniel Gurvič <gurvic.danielius@gmail.com>
2024-09-18 14:43:58 +0300

    Sutvarkyti metodai
    Sutvarkytas įvedimo metodas ir išskaidytas į atskirus vidurkio ir medianos metodus. Įvedimo metodas nuo šiol skirtas tik įvedimui.

--- Struct.cpp
+++ Struct.cpp
@@ -2,16 +2,16 @@
 #include "header.h"
 void ived(Stud & Lok)
 {
-    cout<<"Vardas: "<<endl;
-    cin>>Lok.vardas;
-    cout<<"Pavarde: "<<endl;
-    cin>>Lok.pavarde;
-    double temp;
-
     cout << "Ar norite ivesti pazymius rankiniu budu, ar generuoti atsitiktinai? (Ivesti/Generuoti) " << endl;
     string method;
     cin>>method;
+    //Pasirinkimas ivesti/generuoti/nuskaityti
     if (method=="Generuoti") {
+        cout<<"Vardas: "<<endl;
+        cin>>Lok.vardas;
+        cout<<"Pavarde: "<<endl;
+        cin>>Lok.pavarde;
+        Lok.egz=rand()%10+1;
         cout << "Iveskite pazymiu skaiciu: " << endl;
         int n;
         cin>>n;
@@ -20,32 +20,28 @@ void ived(Stud & Lok)
         }
     }
     else {
+        cout<<"Vardas: "<<endl;
+        cin>>Lok.vardas;
+        cout<<"Pavarde: "<<endl;
+        cin>>Lok.pavarde;
+        double temp;
         cout<<"Iveskite namu darbu rezultatus: "<<endl;
         while (cin>>temp) {
             Lok.ND.push_back(temp);
         }
+        cin.clear();
+        cout << "Iveskite egzamino rezultata: " << endl;
+        cin>>Lok.egz;
     }
 
-    cin.clear();
-    if (choice=="V") {
-        Lok.med=0;
-        Lok.vid=0;
-        for (int i=0;i<Lok.ND.size();i++) {
-            Lok.vid+=Lok.ND.at(i);
-        }
-        Lok.vid=Lok.vid/Lok.ND.size();
 
+    //Pasirinkimas tarp vidurkio ir medianos
+    if (choice=="V") {
+        vidurkis(Lok);
     }
     else {
 
-        Lok.vid=0;
-        sort(Lok.ND.begin(), Lok.ND.end());
-        if (Lok.ND.size()%2==0) {
-            Lok.med=(Lok.ND.at(Lok.ND.size()/2-1)+Lok.ND.at(Lok.ND.size()/2))/2;
-        }
-        else {
-            Lok.med=Lok.ND.at(Lok.ND.size()/2);
-        }
+        mediana(Lok);
     }
 }
 
@@ -66,4 +62,25 @@ void val(Stud & Lok) {
     Lok.vardas.clear();
     Lok.pavarde.clear();
     Lok.ND.clear();
+}
+void vidurkis(Stud &Lok) {
+    Lok.med=0;
+    Lok.vid=0;
+    for (int i=0;i<Lok.ND.size();i++) {
+        Lok.vid+=Lok.ND.at(i);
+    }
+    Lok.vid=Lok.vid/Lok.ND.size();
+    Lok.rez=0.4*Lok.vid+0.6*Lok.egz;
+}
+void mediana(Stud &Lok) {
+    Lok.vid=0;
+    sort(Lok.ND.begin(), Lok.ND.end());
+    if (Lok.ND.size()%2==0) {
+        Lok.med=(Lok.ND.at(Lok.ND.size()/2-1)+Lok.ND.at(Lok.ND.size()/2))/2;
+    }
+    else {
+        Lok.med=Lok.ND.at(Lok.ND.size()/2);
+    }
+    Lok.rez=0.4*Lok.med+0.6*Lok.egz;
+
 }
