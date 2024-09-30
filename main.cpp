ecb6527a83bf87f8b99211b3fcb00ba17eeda807
Daniel Gurvič <gurvic.danielius@gmail.com>
2024-09-30 03:42:53 +0300

    pridėtas laiko matavimas, padaryti globalus kintamieji, nuskaitymo pokyciai

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
 #include "Struct.h"
 #include <limits>
 string choice;
+double nuskaitymo_failo_t, generavimo_failo_t, segregacija_t, sort_t, slabakai_output_t, ramiakai_output_t;
+int lines_num;
 
 int main() {
     vector<Stud> v1;
     Stud Temp;
-    int n,pasirinkimas;
-    string file_choice,output_choice;
-    //Pasirinkimas skaiciuoti vidurki ar mediana
-
+    int pasirinkimas,number_of_nd,number_of_students;
+    string file_choice,output_choice,failo_pavadinimas;
     do {
         cout<<"Meniu:"<<endl;
         cout<<"1:  Skaityti duomenis is failo"<<endl;
@@ -23,66 +23,27 @@ int main() {
             cin.clear();
             cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
-
         switch (pasirinkimas) {
             case 1: {
-                cout<<"Pasirinkite ka skaiciuoti: V - vidurki, M - mediana"<<endl;
+                //TODO kazkur kitur nukisti choice
+                cout<<"Pasirinkite ka skaiciuoti: 1 - vidurki, 2 - mediana"<<endl;
                 while (true) {
                     cin>>choice;
-                    if (choice=="V" || choice=="M") {
+                    if (choice=="1" || choice=="2") {
                         break;
                     }
                     else {
-                        cout<<"Neteisingas pasirinkimas, bandykite dar karta. V-vidurkis, M-mediana"<<endl;
+                        cout<<"Neteisingas pasirinkimas, bandykite dar karta. 1-vidurkis, 2-mediana"<<endl;
                     }
                 }
                 string tekstinis;
-                try{
-                    cout<<"Iveskite failo pavadinima"<<endl;
-                    cin>>tekstinis;
-                    std::ifstream infile(tekstinis);
-                    string eilute;
-                    if (!infile.is_open()) {
-                        throw std::ios_base::failure("Nepavyko atidaryti failo");
-                    }
-
-                    getline(infile,eilute);
-                    istringstream iss(eilute);
-                    vector<string> tokens;
-                    string token;
-                    while(iss>>token) {
-                        tokens.push_back(token);
-                    }
-                    int number_of_nd=tokens.size()-3;
-                    int lines_num=0;
-                    while(getline(infile,eilute)) {
-                        istringstream iss(eilute);
-                        lines_num++;
-                        iss>>Temp.vardas>>Temp.pavarde;
-                        Temp.ND.clear();
-                        int ivertinimas;
-                        for (int i = 0; i < number_of_nd; i++) {
-                            iss >> ivertinimas;
-                            Temp.ND.push_back(ivertinimas);
-                        }
-                        iss >> Temp.egz;
-                        if (choice=="V") {
-                            vidurkis(Temp);
-                        }
-                        else {
-                            mediana(Temp);
-                        }
-                        v1.push_back(Temp);
-                        val(Temp);
-                    }
-                    infile.close();
-                    segregacija(v1);
-                    isvedimas(v1);
-                }
-                catch (const std::ios_base::failure& e) {
-                    std::terminate();
-                }
-
+                cout<<"Iveskite failo pavadinima"<<endl;
+                cin>>tekstinis;
+                auto t1_nuskaitymas=DabartinisLaikas();
+                v1=nuskaitymas_is_failo(tekstinis);
+                auto t2_nuskaitymas=DabartinisLaikas();
+                segregacija(v1);
+                nuskaitymo_failo_t=Skirtumas(t1_nuskaitymas,t2_nuskaitymas);
                 break;
             }
             case 2: {
@@ -98,40 +59,47 @@ int main() {
                 }
                 cout<<"Iveskite studentu skaiciu"<<endl;
                 while(true) {
-                    if (cin>>n) {
+                    if (cin>>lines_num) {
                         break;
                     }
                     cin.clear();
                     cout << "Reikia ivesti skaiciu. Iveskite studentu skaiciu." << endl;
                     cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
                 }
-                for (int i=0;i<n;i++) {
+                for (int i=0;i<lines_num;i++) {
                     ived(Temp);
                     v1.push_back(Temp);
                     val(Temp);
                 }
                 segregacija(v1);
-                isvedimas(v1);
                 break;
             }
-
             case 3:{
-                int number_of_students,number_of_nd;
-                string failo_pavadinimas;
                 cout<<"Iveskite kiek studentu norite sugeneruoti"<<endl;
                 cin>>number_of_students;
                 cout<<"Kiek namu darbu norite sugeneruoti?"<<endl;
                 cin>>number_of_nd;
                 cout<<"Iveskite failo pavadinima"<<endl;
                 cin>>failo_pavadinimas;
+                auto generavimo_t1=DabartinisLaikas();
                 generavimas(number_of_students,failo_pavadinimas,number_of_nd);
-
+                auto generavimo_t2=DabartinisLaikas();
+                generavimo_failo_t=Skirtumas(generavimo_t1,generavimo_t2);
                 break;
-
             }
+            case 10: {
+                cout<<"Failas uzdarytas"<< endl;
         }
+        }
+
     }
     while (pasirinkimas!=10);
+    if (generavimo_failo_t!=0) {
+        cout<<lines_num<<" generavimo laikas: "<<generavimo_failo_t<<"s. "<<endl;
+    }
+    //TODO dar su ivedimu ranka
+    output_times(lines_num,nuskaitymo_failo_t,sort_t,segregacija_t,slabakai_output_t,ramiakai_output_t);
+
     system("pause");
     return 0;
     }
