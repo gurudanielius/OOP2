4fe2f568a13a18c10cd01c1ebe7243d1e7c86a1e
gurudanielius <gurvic.danielius@gmail.com>
2024-11-17 18:18:02 +0200

    Pakeista realizacija i class tipa

--- 3_strategija/vector/Struct.h
+++ 3_strategija/vector/Struct.h
@@ -1,21 +1,33 @@
 #include "header.h"
-    struct Stud {
-        string vardas;
+class Stud {
+	string vardas;
         string pavarde;
         vector<double> ND;
-        double vid;              
-        double med;              
-        double egz;              
-        double rez;              
-    };
+        double egz;
+
+public:
+    double vid;
+    double med;
+    double rez;
+~Stud() { }
+	Stud() : egz(0) { };
+	Stud(std::istream& is);
+	string getVardas() const { return vardas; }
+	string getPavarde() const { return pavarde; }
+	vector<double> getND() const { return ND; }
+	double getEgz() const { return egz; }
+    void setVardas(string vardas) { this->vardas = vardas; }
+	void setPavarde(string pavarde) { this->pavarde = pavarde; }
+	void setND(vector<double> ND) { this->ND = ND; }
+	void setEgz(double egz) { this->egz = egz; }
+ };
     void ived(Stud & Lok);
     void vidurkis(Stud & Lok);
     void output(list <Stud> & Lok);
-    void val(Stud & Lok);
+	void val(Stud & Lok);
     void mediana(Stud & Lok);
     void output2(list<Stud> Lok,const string& file_name);
     void generavimas(int n,string failo_pavadinimasm, int number_of_nd);
-    void isvedimas(list <Stud> Lok);
     void segregacija(list <Stud> &Lok);
     std::chrono::steady_clock::time_point  DabartinisLaikas();
     double Skirtumas(std::chrono::steady_clock::time_point pradzia, std::chrono::steady_clock::time_point pabaiga);
