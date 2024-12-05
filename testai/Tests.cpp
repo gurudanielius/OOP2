aea4c2a7ff7082282f97d26d447aa37a95665f56
Your Name <you@example.com>
2024-12-06 01:40:32 +0200

    Pridetas CopyConstructor testas

--- testai/Tests.cpp
+++ testai/Tests.cpp
@@ -16,3 +16,18 @@ TEST(Studentai, geteriai_seteriai) {
     EXPECT_EQ(student.getPavarde(), "pavarde");
 }
 
+
+TEST(Studentai, CopyKonstruktorius) {
+    Stud student;
+    student.setVardas("vardas");
+    student.setPavarde("pavarde");
+    student.vid = 5;
+    student.med = 6;
+    student.rez = 7;
+    Stud student2 = student;
+    EXPECT_EQ(student2.getVardas(), "vardas");
+    EXPECT_EQ(student2.getPavarde(), "pavarde");
+    EXPECT_EQ(student2.vid, 5);
+    EXPECT_EQ(student2.med, 6);
+    EXPECT_EQ(student2.rez, 7);
+}
