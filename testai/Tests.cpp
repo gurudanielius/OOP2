9e8ecc1dabe4fc40382079e29177e4859f777da4
Your Name <you@example.com>
2024-12-06 01:40:00 +0200

    Pridetas geteriu_seteriu testas

--- testai/Tests.cpp
+++ testai/Tests.cpp
@@ -1,18 +1,18 @@
 #include <gtest/gtest.h>
 #include "../Struct.h"
 
-// Test: Default constructor
-TEST(StudTests, DefaultConstructor) {
+TEST(Studentai, Konstruktorius) {
     Stud student;
     EXPECT_EQ(student.getVardas(), "petras");
     EXPECT_EQ(student.getPavarde(), "petraitis");
     EXPECT_EQ(student.rez, 10);
 }
 
-// Test: File generation
-
-
-
-
-// Test: Sorting
+TEST(Studentai, geteriai_seteriai) {
+    Stud student;
+    student.setVardas("vardas");
+    student.setPavarde("pavarde");
+    EXPECT_EQ(student.getVardas(), "vardas");
+    EXPECT_EQ(student.getPavarde(), "pavarde");
+}
 
