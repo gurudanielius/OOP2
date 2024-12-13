8708215fa186f36eb4c9741966929ba88daf1a92
Daniel Gurvič <gurvic.danielius@gmail.com>
2024-12-13 09:18:54 +0200

    Update Struct.h

--- Struct.h
+++ Struct.h
@@ -69,7 +69,7 @@ public:
 
     void ived(Stud & Lok);
     void vidurkis(Stud & Lok);
-    void test()
+    void testas();
     void output(list <Stud> & Lok);
 	void val(Stud & Lok);
     void mediana(Stud & Lok);
